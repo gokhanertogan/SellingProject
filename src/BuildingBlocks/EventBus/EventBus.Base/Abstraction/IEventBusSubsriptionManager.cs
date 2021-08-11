19d6950fb2a4816ba593338139733101f4b3483f
GokhanErtgn <gokhanertogan@gmail.com>
2021-08-12 00:35:15 +0300

    implement event base

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubsriptionManager.cs
@@ -0,0 +1,31 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+ 
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBusSubsriptionManager
+    {
+        bool IsEmpty { get; }
+
+        event EventHandler<string> OnEventRemoved;
+
+        void AddSubsription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        void RemoveSubsription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
+
+        bool HasSubscriptionsForEvent(string eventName);
+
+        Type GetEventTypeByName(string eventName);
+
+        void Clear();
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+
+        string GetEventKey<T>();
+    }
+}
