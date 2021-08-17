aa9dbc474c8f28c5a9c3ac5ca7c99206bc756f91
GokhanErtgn <gokhanertogan@gmail.com>
2021-08-18 00:06:10 +0300

    implemented order application layer

---
+++ src/Services/OrderService/OrderService.Application/DomainEventHandlers/UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using OrderService.Application.Interfaces.Repositories;
+using OrderService.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.DomainEventHandlers
+{
+    public class UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler :INotificationHandler<BuyerAndPaymentMethodVerifiedDomainEvent>
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        public async Task Handle(BuyerAndPaymentMethodVerifiedDomainEvent buyerAndPaymentMethodVerified, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await orderRepository.GetByIdAsync(buyerAndPaymentMethodVerified.OrderId);
+            orderToUpdate.SetBuyerId(buyerAndPaymentMethodVerified.Buyer.Id);
+            orderToUpdate.SetPaymentMethodId(buyerAndPaymentMethodVerified.Payment.Id);
+        }
+    }
+}
---
+++ src/Services/OrderService/OrderService.Application/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Application.Features.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services, Type startup)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+            //var assm2 = startup.GetTypeInfo().Assembly;
+
+            services.AddAutoMapper(assm);
+            services.AddMediatR(assm);
+            //services.AddMediatR(assm, assm2, typeof(CreateOrderCommandHandler).GetTypeInfo().Assembly);
+
+            return services;
+        }
+    }
+}
